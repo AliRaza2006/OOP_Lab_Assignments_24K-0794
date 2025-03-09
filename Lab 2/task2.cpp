3d685d15be52084ee9815f2aa08b3156fc87e896
AliRaza2006 <maliraza2019@gmail.com>
2025-03-09 14:57:03 +0500

    Update task2.cpp

--- Lab 2/task2.cpp
+++ Lab 2/task2.cpp
@@ -1,27 +1,27 @@
 #include<iostream>
 #include<cstdlib>
 using namespace std;
-void* arrOp(void* arr,int size)
+void* add(char* el[],int size)
 {
-    int* sum = new int(0);
-    for (int i = 0; i < size; i++) {
-        *sum += ((int*)arr)[i];
+    int* c=new int[size];
+    for(int i=0;i<size;i++)
+    c[i]=atoi(el[i]);
+    int* sum=new int(0);
+    for(int i=0;i<size;i++)
+    {
+        *sum+=c[i];
     }
+    delete[] c;
     return (void*)sum;
 }
 int main(int argc,char* argv[])
 {
-    if (argc < 2) 
+    if(argc<2)
     {
-        cout << "Usage: " << *argv << " num1 num2 num3 ..." << endl;
-        return 1;
+        cout<<"Too less arguments!";
+        return 0;
     }
-    int* arr=new int[argc-1];
-    for(int i=0;i<argc-1;i++)
-    {
-        arr[i]=atoi(argv[i+1]);
-    }
-   void* newArr=arrOp((void*) arr,argc-1);
-   cout<<"Sum: "<<*(int*)newArr;
-    delete[] arr;
+    int* result=(int*)add(argv+1,argc-1);
+    cout<<"Sum: "<<*result;
+    delete result;
 }
